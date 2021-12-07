f17b2e6b4bf53ccba239c5dce4cb9f45830cced4
mkoval <mkoval@cinimex.ru>
2021-12-07 16:03:38 +0300

    Сравнение строк исправлено на правильное

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
@@ -21,11 +21,11 @@ public class ApplicationManager {
   }
 
   public void init() {
-    if (browser == Browser.FIREFOX.browserName()) {
+    if (browser.equals(Browser.FIREFOX.browserName())) {
       wd = new FirefoxDriver();
-    } else if (browser == Browser.CHROME.browserName()){
+    } else if (browser.equals(Browser.CHROME.browserName())){
       wd = new ChromeDriver();
-    } else if (browser == Browser.IE.browserName()){
+    } else if (browser.equals(Browser.IE.browserName())){
       wd = new InternetExplorerDriver();
     }
 
