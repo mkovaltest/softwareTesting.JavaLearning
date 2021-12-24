7fe5c930d185d0ca664e4640e111001dbafebce4
mkoval <mkoval@cinimex.ru>
2021-12-24 19:14:55 +0300

    Лекция 4.6. Сравнение коллекций

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTests.java
@@ -22,5 +22,6 @@ public class GroupModificationTests extends TestBase{
     app.getGroupHelper().returnToGroupPage();
     List<GroupData> after = app.getGroupHelper().getGroupList();
     Assert.assertEquals(after.size(), before.size());
+    Assert.assertEquals(before, after);
   }
 }
