9126409feae18fb27def9fbe50efb2e0c3a9664b
mkoval <mkoval@cinimex.ru>
2022-01-17 23:01:44 +0300

    Задание №14: Вынести описание тестового стенда в конфигурационный файл

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
@@ -6,9 +6,12 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.remote.Browser;
 
+import java.io.*;
+import java.util.Properties;
 import java.util.concurrent.TimeUnit;
 
 public class ApplicationManager {
+  private final Properties properties;
   private NavigationHelper navigationHelper;
   private GroupHelper groupHelper;
   private SessionHelper sessionHelper;
@@ -18,9 +21,12 @@ public class ApplicationManager {
 
   public ApplicationManager(String browser) {
     this.browser = browser;
+    properties = new Properties();
   }
 
-  public void init() {
+  public void init() throws IOException {
+    String target = System.getProperty("target", "local");
+    properties.load(new FileReader(String.format("src/test/resources/%s.properties", target)));
     if (browser.equals(Browser.FIREFOX.browserName())) {
       wd = new FirefoxDriver();
     } else if (browser.equals(Browser.CHROME.browserName())){
@@ -28,14 +34,14 @@ public class ApplicationManager {
     } else if (browser.equals(Browser.IE.browserName())){
       wd = new InternetExplorerDriver();
     }
-    
+
     wd.manage().timeouts().implicitlyWait(0, TimeUnit.SECONDS);
-    wd.get("http://localhost/addressbook/");
+    wd.get(properties.getProperty("web.baseUrl"));
     groupHelper = new GroupHelper(wd);
     navigationHelper = new NavigationHelper(wd);
     sessionHelper = new SessionHelper(wd);
     contactHelper = new ContactHelper(wd);
-    sessionHelper.login("admin", "secret");
+    sessionHelper.login(properties.getProperty("web.adminLogin"),properties.getProperty("web.adminPassword"));
   }
 
   public void stop() {
--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
@@ -5,12 +5,15 @@ import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeSuite;
 import ru.stqa.pft.addressbook.appmanager.ApplicationManager;
 
+import java.io.IOException;
+
 public class TestBase {
 
-  protected static final ApplicationManager app = new ApplicationManager(Browser.CHROME.browserName());
+  protected static final ApplicationManager app
+          = new ApplicationManager(System.getProperty("browser", Browser.CHROME.browserName()));
 
   @BeforeSuite
-  public void setUp() {
+  public void setUp() throws IOException {
     app.init();
   }
 
