0dd1468969f05afc2438c8aa36003b5dd3ed2ed4
mkoval <mkoval@cinimex.ru>
2022-01-26 02:36:03 +0300

    Лекция 9.6. REST: взаимодействие с GitHub

---
+++ github-sample/src/test/java/ru/stqa/pft/github/GithubTests.java
@@ -0,0 +1,19 @@
+package ru.stqa.pft.github;
+
+import com.google.common.collect.ImmutableMap;
+import com.jcabi.github.*;
+import org.testng.annotations.Test;
+
+import java.io.IOException;
+
+public class GithubTests {
+
+  @Test
+  public void testCommits() throws IOException {
+    Github github = new RtGithub("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");
+    RepoCommits commits = github.repos().get(new Coordinates.Simple("mkovaltest", "softwareTesting.JavaLearning")).commits();
+    for (RepoCommit commit : commits.iterate(new ImmutableMap.Builder<String, String>().build())){
+      System.out.println(new RepoCommit.Smart(commit).message());
+    }
+  }
+}
